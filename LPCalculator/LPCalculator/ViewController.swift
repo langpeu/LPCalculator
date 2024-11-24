19dc17013326c6f0d986de5e3ee33b02b1538d82
langpeu <icopy@naver.com>
2024-11-24 22:28:02 +0900

    chore: init 프로젝트 생성

---
+++ LPCalculator/LPCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LPCalculator
+//
+//  Created by Langpeu on 11/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
